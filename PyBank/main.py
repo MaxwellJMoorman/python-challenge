4c27c9dcb0fbcdc6d7547480b60ac223a0ed8f77
MaxwellJMoorman <70612783+MaxwellJMoorman@users.noreply.github.com>
2020-09-30 09:37:39 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,47 @@
-#
+# PyBank
+
+# Load CSV and os
+import csv
+
+# Create Lists
+total_months = []
+total_profit = []
+date = []
+monthly_profit_change = []
+
+# Open CSV
+with open('budget_data.csv') as csvDataFile:
+    csvreader = csv.reader(csvDataFile)
+    csv_header = next(csvDataFile)
+
+    # Assign data into lists
+    for row in csvreader:
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+    
+        # Find net profit
+        net_profit = sum(total_profit)
+        date.append(row[0])
+
+        #Find difference in dates
+        date_increase = total_profit.index(max(total_profit))
+        date_decrease = total_profit.index(min(total_profit))
+    
+    for rows in range(len(total_profit)-1):
+        monthly_profit_change.append(total_profit[rows+1]-total_profit[rows])
+    
+        # Print results
+        print('Financial Analysis')
+        print('---------------------')
+        print('Total Months: {}'.format(len(total_months)))
+        print('Net Profit ${}'.format(net_profit))
+        print('Greatest Increase in Profits (${}) {}'.format(max(monthly_profit_change),(date[date_increase])))
+        print('Greatest decrease in Profits (${}) {}'.format(min(monthly_profit_change),(date[date_decrease])))
+    
+
+
+    
+
+    
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,34 @@
-##
+### PyPoll
+
+# Load CSV and os
+import csv
+
+# Create Lists
+total_votes = []
+whom_vote = []
+Unique_ID = []
+
+# Open CSV
+with open('election_data.csv') as csvDataFile:
+    csvreader = csv.reader(csvDataFile)
+    csv_header = next(csvDataFile)
+
+    # Assign data into lists
+    for row in csvreader:
+        total_votes.append(row[0])
+        whom_vote.append((row[2]))
+        
+        #
+        if row[2] not in Unique_ID:
+            Unique_ID.append(row[2])    
+
+    #Print Results
+    print('Election Result')
+    print('Total # of Votes: {}'.format(len(total_votes)))
+    for name in Unique_ID:
+        print('{}: {:.3f}% ({})'.format(name,(whom_vote.count(name)/len(total_votes))*100,whom_vote.count(name)))        
+
+    
+
+
+    
